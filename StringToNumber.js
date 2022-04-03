0f58f23320029fc9c3d953a82694e0fed5b238c3
francis kamande <franciskamande2001@gmail.com>
2022-04-03 14:09:08 +0300

    Inbuilt function to convert stringType numbers into IntegerTYpe numbers

---
+++ StringToNumber.js
@@ -0,0 +1,6 @@
+//coverting a string to a number
+    console.log("\nString to Number");
+    function stringToNumber(val){
+        return parseInt(val,2);
+    }
+    console.log(stringToNumber(111011));
