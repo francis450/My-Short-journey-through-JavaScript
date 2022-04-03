1d926c76c156d505a902221874f285943c1ec85d
francis kamande <franciskamande2001@gmail.com>
2022-04-03 14:06:52 +0300

    Physical implementation of the class blueprint

---
+++ Objects.js
@@ -0,0 +1,11 @@
+    //Objects
+    console.log("\nObjects:");
+    var me = {
+        "name":"Francis",
+        "my age" : 21,
+        "height" : 56,
+        "background" : "college" 
+    }
+    console.log(me.name);//dot notation -> object access
+    let age = me["my age"];//bracket notation
+    console.log(me.name+"'s age: "+age);
