28828d78d9d07613e99b09c835153dbec0afecf0
francis kamande <franciskamande2001@gmail.com>
2022-04-04 01:18:29 +0300

    Normal functions architecture

---
+++ ArrowFunction.js
@@ -0,0 +1,9 @@
+//Arrow functions
+console.log("\nArrow functions");
+//original function
+var magic = function (){
+    return new Date();
+};
+var myCancat = function(arr1,arr2){
+    return arr1.concat(arr2);
+};
