a4309717d950ad2e7244f9631e13c4fd199fa1a4
francis kamande <franciskamande2001@gmail.com>
2022-04-09 16:32:59 +0300

    Initializing the hash function

--- hashTables.js
+++ hashTables.js
@@ -1,3 +1,21 @@
 <script>
+//Hash table
+
+	//The hash function
+	//string to be hashed and max is the number of buckets used in the function to store values
+	var hash = (string,max) =>{
+		var hash = 0;
+		for(var i = 9;i < string.length; i++){
+			hash +=string.charCodeAt(i);
+		} 
+		return hash%max;
+	};
+
 	
+
+
+
+
+
+
 </script>
