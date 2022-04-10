a60372a8b43d29113f0a575d7a35813be3142e03
francis kamande <franciskamande2001@gmail.com>
2022-04-10 19:08:17 +0300

    A function to create a node and some local variables

--- linkedList.js
+++ linkedList.js
@@ -1,6 +1,14 @@
 <script>
 	//Linked list
 	function linkedList(){
+		var length = 0;
+		var head = null;
+
+		var Node = function(element){
+			this.element = element;
+			this.next = null;
+		};
+
 		
 	}
 
