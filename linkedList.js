c701eb2088160c1768160f0e36e4193536695e00
francis kamande <franciskamande2001@gmail.com>
2022-04-11 18:35:50 +0300

    Function to add a node to the list

--- linkedList.js
+++ linkedList.js
@@ -4,14 +4,39 @@
 		var length = 0;
 		var head = null;
 
+		//A function to create a node
 		var Node = function(element){
 			this.element = element;
 			this.next = null;
 		};
 
+		//to determine the number of nodes on the list
 		this.size = function(){
 			return length;
+		};
+
+		//function to return the head node
+		this.head = function(){
+			return head;
 		}
+
+		//function to add a node to the list
+		this.add  = function(){
+			var node = new Node(element);
+			if(head === null){
+				head = node;
+			}else{
+				currentNode = head;
+
+				while(currentNode.next){
+					currentNode = currentNode.next;
+				} 
+
+				currentNode.next = node;
+			}
+
+			length++;
+		};
 	}
 
 
